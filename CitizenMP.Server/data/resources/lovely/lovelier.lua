038a9ba7679156bef1666a526516e03b54342908
NTAuthority <bas@fourdeltaone.net>
2014-05-14 11:24:05 +0200

    remove some more debug prints

--- CitizenMP.Server/data/resources/lovely/lovelier.lua
+++ CitizenMP.Server/data/resources/lovely/lovelier.lua
@@ -18,22 +18,10 @@ AddEventHandler('svPrint', function(m)
     print(tostring(source) .. ' says ' .. m)
 
     CreateRPCContext(source, function()
-        print('making RPC context')
-
         return function()
-            print('in RPC context')
-
             local ped = GetPlayerChar(0, _i)
-
-            print('in RPC context#')
-
             local x, y, z = GetCharCoordinates(ped, _f, _f, _f)
-
-            print('in RPC context##')
-
             PrintStringWithLiteralStringNow("STRING", "z: " .. z(), 5500, true)
-
-            print('in RPC context###')
         end
     end)
 end)
