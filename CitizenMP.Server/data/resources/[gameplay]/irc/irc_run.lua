2490e586fce110a1e739f055328dee3abaddc9f1
icedream <icedream2k9@die-optimisten.net>
2015-03-14 23:16:45 +0100

    irc: Don't display "left the server" message if the "joined the server" message never appeared for a player.

--- CitizenMP.Server/data/resources/[gameplay]/irc/irc_run.lua
+++ CitizenMP.Server/data/resources/[gameplay]/irc/irc_run.lua
@@ -2,6 +2,9 @@ local chatSharp = clr.ChatSharp
 
 local client = chatSharp.IrcClient('irc.rizon.net', chatSharp.IrcUser('mateyate', 'mateyate'), false)
 
+-- temporary workaround for connections that never triggered playerActivated but triggered playerDropped
+local activatedPlayers = {}
+
 client.ConnectionComplete:add(function(s : object, e : System.EventArgs) : void
 	client:JoinChannel('#fourdeltaone')
 end)
@@ -15,16 +18,30 @@ end)
 
 AddEventHandler('playerActivated', function()
 	client:SendMessage('* ' .. GetPlayerName(source) .. '(' .. GetPlayerGuid(source) .. '@' .. GetPlayerEP(source) .. ') joined the server', '#fourdeltaone')
+	table.insert(activatedPlayers, GetPlayerGuid(source))
 end)
 
 AddEventHandler('playerDropped', function()
-	client:SendMessage('* ' .. GetPlayerName(source) .. '(' .. GetPlayerGuid(source) .. '@' .. GetPlayerEP(source) .. ') left the server', '#fourdeltaone')
+	-- find out if this connection ever triggered playerActivated
+	for index,guid in pairs(activatedPlayers) do
+		if guid == playerGuid then
+			-- show player dropping connection in chat
+			client:SendMessage('* ' .. GetPlayerName(source) .. '(' .. GetPlayerGuid(source) .. '@' .. GetPlayerEP(source) .. ') left the server', '#fourdeltaone')
+			table.remove(activatedPlayers, index)
+			return
+		end
+	end
 end)
 
 AddEventHandler('chatMessage', function(source, name, message)
 	print('hey there ' .. name)
 
 	local displayMessage = gsub(message, '^%d', '')
+
+	-- ignore zero-length messages
+	if string.len(displayMessage) == 0 then
+		return
+	end
 
 	-- ignore chat messages that are actually commands
 	if string.sub(displayMessage, 1, 1) == "/" then
