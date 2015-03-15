ec2495143fa4d3080af72116324e15e486b5587c
icedream <icedream2k9@die-optimisten.net>
2015-03-15 08:27:43 +0100

    irc: Broadcast player deaths.

--- CitizenMP.Server/data/resources/[gameplay]/irc/irc_run.lua
+++ CitizenMP.Server/data/resources/[gameplay]/irc/irc_run.lua
@@ -51,6 +51,37 @@ AddEventHandler('chatMessage', function(source, name, message)
 	client:SendMessage('[' .. tostring(GetPlayerName(source)) .. ']: ' .. displayMessage, '#fourdeltaone')
 end)
 
+AddEventHandler('onPlayerKilled', function(playerId, attackerId, reason, position)
+    local player = GetPlayerByServerId(playerId)
+    local attacker = GetPlayerByServerId(attackerId)
+
+    local reasonString = 'killed'
+
+    if reason == 0 or reason == 56 or reason == 1 or reason == 2 then
+        reasonString = 'meleed'
+    elseif reason == 3 then
+        reasonString = 'knifed'
+    elseif reason == 4 or reason == 6 or reason == 18 or reason == 51 then
+        reasonString = 'bombed'
+    elseif reason == 5 or reason == 19 then
+        reasonString = 'burned'
+    elseif reason == 7 or reason == 9 then
+        reasonString = 'pistoled'
+    elseif reason == 10 or reason == 11 then
+        reasonString = 'shotgunned'
+    elseif reason == 12 or reason == 13 or reason == 52 then
+        reasonString = 'SMGd'
+    elseif reason == 14 or reason == 15 or reason == 20 then
+        reasonString = 'assaulted'
+    elseif reason == 16 or reason == 17 then
+        reasonString = 'sniped'
+    elseif reason == 49 or reason == 50 then
+        reasonString = 'ran over'
+    end
+
+	client:SendMessage('* ' .. attacker.name .. ' ' .. reasonString .. ' ' .. player.name, '#fourdeltaone')
+end)
+
 client:ConnectAsync()
 
 AddEventHandler('onResourceStop', function(name)
